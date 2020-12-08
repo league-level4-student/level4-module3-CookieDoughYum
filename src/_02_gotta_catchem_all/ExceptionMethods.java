746b8b9610db0b0349a9c521d84cab87a221a009
root <root@f9d418b88c88>
2020-12-08 00:15:30 +0000

    Finished module 3.

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,5 +1,21 @@
 package _02_gotta_catchem_all;
 
 public class ExceptionMethods {
-	
+	 Double divide(double one, double two) {
+		 if(two==0.0) {
+			 throw new IllegalArgumentException();
+		 }
+		return one/two;
+	}
+	 
+	 String reverseString(String a) {
+		 String reverse="";
+		 for(int i=a.length()-1; i>0; i--) {
+			 reverse+=a.charAt(i);
+		 }
+		 if(a.equals("")) {
+			 throw new IllegalStateException();
+		 }
+		 return reverse;
+	 }
 }
